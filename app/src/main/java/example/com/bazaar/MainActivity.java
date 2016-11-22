b64c60502bba5173a8f030e4beb62e490a76b539
suman1shrestha <sumanshrestha@Sumans-MacBook-Pro-2.local>
2016-11-22 00:10:19 -0600

    First commit

---
+++ app/src/main/java/example/com/bazaar/MainActivity.java
@@ -0,0 +1,26 @@
+package example.com.bazaar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goLogin(View view)
+    {
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
+    }
+
+    public void goRegister(View view){
+        Intent register = new Intent(this, RegisterActivity.class);
+        startActivity(register);
+    }
+}
---
+++ app/src/main/java/example/com/bazaar/SignInActivity.java
@@ -0,0 +1,31 @@
+package example.com.bazaar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+
+    public void attemptLogin(View view){
+        EditText getUser = (EditText) findViewById(R.id.username);
+        EditText getPass = (EditText) findViewById(R.id.password);
+
+        String username = getUser.getText().toString();
+        String password = getPass.getText().toString();
+
+        Intent loggedIn = new Intent(this, Home.class);
+        startActivity(loggedIn);
+//        if(username.compareToIgnoreCase("admin")==1 && password.compareToIgnoreCase("admin")==1){
+//            Intent loggedIn = new Intent(this, Home.class);
+//            startActivity(loggedIn);
+//        }
+    }
+}
