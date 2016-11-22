c74de676af7ac71b1081d435cd24a63430fc79ed
Sosal Pokhrel <Sosal@Sosals-MacBook-Pro.local>
2016-11-22 00:20:30 -0600

    files for linking added

---
+++ app/src/main/java/example/com/bazaar/Memberinfo.java
@@ -0,0 +1,13 @@
+package sosal.payment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Memberinfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memberinfo);
+    }
+}
---
+++ app/src/main/java/example/com/bazaar/PaymentMainActivity.java
@@ -0,0 +1,28 @@
+package sosal.payment;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class PaymentMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_main);
+    }
+
+    public void goCash(View view){
+        Intent cash = new Intent(this, Cash.class);
+        startActivity(cash);
+    }
+   public void goCredit(View view)
+    {
+    Intent credit =new Intent(this,Credit.class);
+        startActivity(credit);
+    }
+
+
+
+}
