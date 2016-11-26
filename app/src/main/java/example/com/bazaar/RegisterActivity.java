be856e67b58303433f4f645393ea27ce32af04e2
Shrey Aryal <TheRealShrey@Shreys-MacBook-Pro.local>
2016-11-25 20:04:00 -0600

    Profile picture upload to firebase

--- app/src/main/java/example/com/bazaar/RegisterActivity.java
+++ app/src/main/java/example/com/bazaar/RegisterActivity.java
@@ -1,16 +1,32 @@
 package example.com.bazaar;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -24,8 +40,15 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText password;
     private ArrayList<UserInfo> users;
 
+    //For Image Storage
+    private Button mSelectImage;
+    private StorageReference mStorage;
+
     String user;
     String pass;
+
+
+    private static final int GALLERY_INTENT = 2;
 
     public RegisterActivity() {
         users = new ArrayList<>();
@@ -44,7 +67,8 @@ public class RegisterActivity extends AppCompatActivity {
                 Iterator<DataSnapshot> iterator = snapshotIterator.iterator();
                 System.out.println(dataSnapshot.getChildren().getClass().toString());
                 while (iterator.hasNext()) {
-                   GenericTypeIndicator<ArrayList<UserInfo>> t = new GenericTypeIndicator<ArrayList<UserInfo>>() {};
+                    GenericTypeIndicator<ArrayList<UserInfo>> t = new GenericTypeIndicator<ArrayList<UserInfo>>() {
+                    };
                     users = iterator.next().getValue(t);
                 }
             }
@@ -58,14 +82,32 @@ public class RegisterActivity extends AppCompatActivity {
         username = (EditText) findViewById(R.id.input_username);
         password = (EditText) findViewById(R.id.input_password);
 
+
+        //For profile Picture
+
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        mSelectImage = (Button) findViewById(R.id.buttonSelectImage);
+
+        //final StorageReference mountainsRef = mStorage.child("images");
+        mSelectImage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view)
+            {
+              Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, GALLERY_INTENT);
+
+            }
+
+        });
+
     }
+
 
     public void registerUser(View view) {
 
         user = username.getText().toString();
         pass = password.getText().toString();
-
-        System.out.println("password is: "+pass);
         DatabaseReference usersData = bazaar.child("User");
 
         UserInfo temp = new UserInfo();
@@ -79,4 +121,42 @@ public class RegisterActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SignInActivity.class);
         startActivity(intent);
     }
+
+
+//    public void loadImagefromGallery(View view) {
+//        // Create intent to Open Image applications like Gallery, Google Photos
+//        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+//                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//        // Start the Intent
+//        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+//    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // When an Image is picked
+        if (requestCode == GALLERY_INTENT && resultCode == RESULT_OK
+                && null != data) {
+            // Get the Image from data
+
+            Uri selectedImage = data.getData();
+            StorageReference filePath = mStorage.child("Photos").child(selectedImage.getLastPathSegment());
+//                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            filePath.putFile(selectedImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(RegisterActivity.this, "Upload Done.", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Handle unsuccessful uploads
+                    Toast.makeText(RegisterActivity.this, "Upload Unsuccessful.", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
+        }
+    }
 }
+
